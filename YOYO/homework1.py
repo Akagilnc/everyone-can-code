a68e41260fbcb69efc0c851167a982d7877d2ff5
YOYO1HU9 <121189275@qq.com>
2018-03-31 22:33:07 +0800

    commit message

--- YOYO/homework1.py
+++ YOYO/homework1.py
@@ -6,6 +6,19 @@ ran_num = random.randint(minimal, maximal)
 
 information =  "please choose a number from {} to {} : " .format(minimal, maximal)
 my_number = int(input(information))
+x = 1
 
-if my_number == ran_num :
-   print("you got it at the first time")
+while True :
+   if   my_number == ran_num :
+        print("you got it at the {} time".format(x))
+        break
+   if   my_number > ran_num :
+        print("smaller please")
+   if   my_number < ran_num :
+        print("larger please")
+   x = x + 1
+   my_number = int(input(information))
+   if   my_number == ran_num :
+        print("ÿou got it at the {} times.".format(x))
+        break
+
