ae02572e5a22d4afc55ba81a61f0fbcbdf480046
YOYO1HU9 <121189275@qq.com>
2018-03-31 22:04:56 +0800

    commit message

--- YOYO/homework1.py
+++ YOYO/homework1.py
@@ -3,3 +3,9 @@ import random
 minimal = 1
 maximal = 10
 ran_num = random.randint(minimal, maximal)
+
+information =  "please choose a number from {} to {} : " .format(minimal, maximal)
+my_number = int(input(information))
+
+if my_number == ran_num :
+   print("you got it at the first time")
