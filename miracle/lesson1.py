80db7f792cf5e553b40fc6471b6f1f88087c8178
DeronLee <akagilnc@gmail.com>
2018-04-27 17:30:57 +0900

    fixed the bugs in miracle/lesson1

--- miracle/lesson1.py
+++ miracle/lesson1.py
@@ -1,21 +1,20 @@
+input_number = input()
+input_number = int(x)
 
-
-
-x = input()
-x = int(x)
 
 # 希望输入一个数字，返回这个数字的平方根
-def sq(input_number):
-    #猜测一个数字g
+def sq(x):
+    # 猜测一个数字g
     g = 3
 
     while True:
-        #比较g*g 是否等于 x
-        if g*g == x:
-            #如果相等，返回结果g
+        # 比较g*g 是否等于 x
+        if g * g == x:
+            # 如果相等，返回结果g
             return g
         else:
-            #如果不等于g=(x/g + g)/2
-            g = (x/g + g)/2
+            # 如果不等于g=(x/g + g)/2
+            g = (x / g + g) / 2
 
-print(sq(x))
+
+print(sq(input_number))
