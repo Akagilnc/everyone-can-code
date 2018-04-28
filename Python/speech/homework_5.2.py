8a60744aa6752b520378b95a836e33744f38a870
YOYO1HU9 <121189275@qq.com>
2018-04-28 11:22:22 +0800

    def 3

--- Python/speech/homework_5.2.py
+++ Python/speech/homework_5.2.py
@@ -5,12 +5,24 @@ def number():
         return "it is a even"
     return "it is a odd number"
 #函数二：计算字符串长度
-def length():
-     var = "Have fun"
+def length(abc):
+     var = "abc"
      if type(var) == str :
          return length(var)
      else :
          return "invalid string"
+#函数三：利用穷举法计算立方根
+def cube():
+#列出初始数字，每次的变量，精度
+     basic, x, y,number = 1, 0.001, 0.0001, 8
+#计算和比较
+     if abs(number - basic**3) > y :
+         basic += 1
+     else :
+         return basic
+
+
+
 
 
 
