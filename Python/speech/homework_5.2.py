9b430852f964b98f9dcf5672a53efe1bd1d8e2db
YOYO1HU9 <121189275@qq.com>
2018-04-28 11:37:37 +0800

    test and correction

--- Python/speech/homework_5.2.py
+++ Python/speech/homework_5.2.py
@@ -1,25 +1,32 @@
 #函数一：判断奇数和偶数
-def number():
-    num = int(input("input a number"))
+def number(num):
     if num % 2 == 0 :
         return "it is a even"
     return "it is a odd number"
 #函数二：计算字符串长度
-def length(abc):
-     var = "abc"
+def length(var):
      if type(var) == str :
-         return length(var)
+         return int(len(var))
      else :
          return "invalid string"
 #函数三：利用穷举法计算立方根
-def cube():
+def cube(number):
 #列出初始数字，每次的变量，精度
-     basic, x, y,number = 1, 0.001, 0.0001, 8
+     basic, x, y = 1, 0.001, 0.0001
 #计算和比较
-     if abs(number - basic**3) > y :
+     while abs(number - basic**3) > y :
          basic += 1
      else :
-         return basic
+         return int(basic)
+#测试程序运行
+num = int(input("input a number :"))
+print(number(num))
+
+var = "hello"
+print(length(var))
+
+number = 8
+print(cube(number))
 
 
 
