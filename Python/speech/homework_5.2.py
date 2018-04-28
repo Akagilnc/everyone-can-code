db5e175b4f494fe2ae0e4dbca09f8ebb86015265
YOYO1HU9 <121189275@qq.com>
2018-04-28 11:04:54 +0800

    def 2

--- Python/speech/homework_5.2.py
+++ Python/speech/homework_5.2.py
@@ -4,3 +4,14 @@ def number():
     if num % 2 == 0 :
         return "it is a even"
     return "it is a odd number"
+#函数二：计算字符串长度
+def length():
+     var = "Have fun"
+     if type(var) == str :
+         return length(var)
+     else :
+         return "invalid string"
+
+
+
+
