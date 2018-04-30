f305fcc51411414ce88ee2574394f2ca9d8274f7
mimiracle@108 <1084242475@qq.com>
2018-04-30 17:59:53 +0800

    h5.py

--- miracle/h5.py
+++ miracle/h5.py
@@ -1,13 +1,35 @@
 ##!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def ji_ou(a):
-    # 判断x是否为偶数
+# function 1
+def even_odd(a):
+    # 判断a是否为偶数
     if a % 2 == 0:
         # 返回“it's an even"
         return "it's an even" 
     #返回“it's an odd"
     return "it's an odd"
 
-a=5
-print ji-ou(5)
+# function 2
+def length(sentence)
+    # 判断是否为字符串
+    if type(sentence) == str :
+        #返回字符串的长度
+        return len(shentence)
+    #返回“invalid string”
+    return "invalid string"
+    
+
+
+
+# fuction 3
+def cube_root(g)
+    # 定义精度，定义变量，数字
+    epi, x, n = 0.00001, 0.001，1
+    low, high = 1 
+    # 如果 猜测平方根 ** 3 - g > 精度 重复执行下面的步骤
+    while abs(n**3 - g) > epi:
+        # 猜测立方根 += step
+
+
+
