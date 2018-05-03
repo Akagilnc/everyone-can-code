6ad953b6274cff20ebf0b5f4923ae4547592d2c0
DeronLee <akagilnc@gmail.com>
2018-05-03 20:04:51 +0900

    restyle

--- Python/homework4.2_yoyo.py
+++ Python/homework4.2_yoyo.py
@@ -1,24 +1,30 @@
-#函数一：判断奇数和偶数
+# 函数一：判断奇数和偶数
 def even_odd(num):
-    if num % 2 == 0 :
+    if num % 2 == 0:
         return "it is a even"
     return "it is an odd number"
-#函数二：计算字符串长度
-def  string_not(var):
-     if type(var) == str :
-         return int(len(var))
-     else :
-         return "invalid string"
-#函数三：利用穷举法计算立方根
+
+
+# 函数二：计算字符串长度
+def string_not(var):
+    if type(var) == str:
+        return int(len(var))
+    else:
+        return "invalid string"
+
+
+# 函数三：利用穷举法计算立方根
 def cube(number):
-#列出初始数字，每次的变量，精度
-     basic, x, y = 1, 0.001, 0.0001
-#计算和比较
-     while abs(number - basic**3) > y :
-         basic += x
-     else :
-         return int(basic)
-#测试程序运行
+    # 列出初始数字，每次的变量，精度
+    basic, x, y = 1, 0.001, 0.0001
+    # 计算和比较
+    while abs(number - basic ** 3) > y:
+        basic += x
+    else:
+        return int(basic)
+
+
+# 测试程序运行
 num = int(input("input a number :"))
 print(number(num))
 
@@ -27,10 +33,3 @@ print(length(var))
 
 number = 8
 print(cube(number))
-
-
-
-
-
-
-
