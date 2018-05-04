ea1d888deb7424d5c5de59eb7bc8fce2345f30f6
DeronLee <akagilnc@gmail.com>
2018-05-04 11:14:03 +0900

    class lesson

---
+++ Python/person.py
@@ -0,0 +1,13 @@
+from sq_class import sq
+class robot():
+    height, age, name = 0, 0, ""
+    emotion = ['happy', 'sad', 'angry', 'happiness']
+
+    def __init__(self, height, age, name):
+        self.height = height
+        self.age = age
+        self.name = name
+        
+
+print(sq(4))
+
