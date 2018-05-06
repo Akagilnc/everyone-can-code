a1995fe69da838eeb44c09ac96ec36bd9048f68b
YOYO1HU9 <121189275@qq.com>
2018-05-06 16:37:49 +0800

    longest sustring

--- YOYO/homework_6.2.py
+++ YOYO/homework_6.2.py
@@ -23,4 +23,29 @@ def times_of_bob(s):
 s = "azcbobobegghakl"
 print ("Number of times bob occurs is :" , times_of_bob(s))
 
+#统计最长的子字符串
+import re
+#按字母顺序排序
+def longest_substring(s):
+    alphabetical_order = r'a?b?c?d?e?f?g.h?i?j?k?l?'
+    m = re.findall(alphabetical_order,s)
+#对返回的list进行排序
+    m = sorted(m, key=len, reverse=True)
+#多项时返回第一项
+    return m[0]
+#测试程序
+s = "azcbobobegghakl"
+print("Longest substring in alphabetical order is :" , longest_substring(s))
 
+import re
+#按字母顺序排序
+def longest_substring(s):
+    alphabetical_order = r'a?b?c?d?e?f?g?h?i?j?k?l?'
+    m = re.findall(alphabetical_order,s)
+#对返回的list进行排序
+    m = sorted(m, key=len, reverse=True)
+#多项时返回第一项
+    return m[0]
+#测试程序
+s = "abcbcd"
+print("Longest substring in alphabetical order is :" , longest_substring(s))
