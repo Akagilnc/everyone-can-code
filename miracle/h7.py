3bfeeb5dbbaa0a6f926bfbd6511c948b405ac128
mimiracle@108 <1084242475@qq.com>
2018-05-07 21:38:36 +0800

    h7.py

--- miracle/h7.py
+++ miracle/h7.py
@@ -1,54 +1,41 @@
 ##!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 搞了半天还是无法运行程序，一直找不到文件，也没法验证检查。我先上传了，再琢磨下再改。
-# function 1
-def stats_vowel(s):
-    s = 'azcbobobegghakl'
-    num = 0
-    for s in 'aeiou':
-        num +=1
-print ("numbers of vowels:" + str(num))
-
-# function 2
-def finds(s1,s2):
-    sum = 0
-    for i in range (0,len(s2)):
-        str = s2[i:]
-        if(str.find(s1) == 0):
-            sum +=1
-        else:
-            continue
+# 统计元音个数
+def count_vowel(s):
+    # 元音字母总个数
+    count = 0
+    for letter in s:
+        if letter=='a' or letter=='e' or letter=='i' or letter=='o' or letter=='u':
+            count += 1
 # 测试
-s1 = 'bob'
-s2 = 'azcbobobegghakl'
-print (finds(s1,s2))
+s ='azcbobobegghakl'           
+print ("numbers of vowels:" + count_vowel(s))
 
-# function 3
-def max_sub(s):
-    cont =[]
-    sub =[]
-    for i in s :
-        if len(sub) >=1 and lowercase.index(sub[-1]) <= lowercase.index(i):
-            sub.append(i)
-            cont.append(''.join(sub))
-        else:
-            sub = []
-            sub.append(i)
-    # print cont
-    cont = sorted(cont,key=len,reverse=True)
-    return cont [0]
-
+# 统计“bob次数”
+def num_bob(str):
+    result = [str[i:i+3] for i in range(len(str)-2)]
+    return result.count('bob')
 # 测试
-s1 = 'azcboboegghakl'
-s2 ='abcdcd'
-result 1 = max_sub(s1)
-print "longest subsrting of "azcboboegghakl" in alphabetival order is "begg" (s1,result1)
-result 2 = max_sub(s2)
-print "longest subsrting of "abcdcd" in alphabetival order is "abc" (s2,result2)
+str = 'azcbobobegghakl'
+print ("Number of times bob occurs is :" + num_bob(str))
 
-
-
+# 最长字符
+def longest_substring(s):
+    in_orders = ['' for i in range(len(s))]
+    index = 0
+    for i in range(len(s)):
+        if (i == len(s) - 1 and s[i] >= s[i - 1]) or s[i] <= s[i + 1]:
+            in_orders[index] += s[i]
+        else:
+            in_orders[index] += s[i]
+            index += 1
+    return max(in_orders, key=len)  
+# 测试
+s1 = 'azcbobobegghakl' 
+print ("Longest substring in alphabetical order is: " + longest_substring(s))
+s2 = 'abcbcd'  
+print ("Longest substring in alphabetical order is: " + longest_substring(s))
    
 
 
