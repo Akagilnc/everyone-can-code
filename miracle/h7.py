2393f3fad295de8981005839ad09a0e3d14d03ee
mimiracle@108 <1084242475@qq.com>
2018-05-08 21:39:07 +0800

    h7.hy

--- miracle/h7.py
+++ miracle/h7.py
@@ -4,6 +4,7 @@
 # 统计元音个数
 def num_of_vowels(s):
     count = 0
+    # 利用in的方法查找字符串中元音的数量
     for letter in s:
         if letter in 'aeiou':
             count += 1
@@ -14,8 +15,13 @@ print ("Numbers of vowels: " , num_of_vowels(s))
 
 # 统计“bob次数”
 def num_bob(str):
-    result = [str[i:i+3] for i in range(len(str)-2)]
-    return result.count('bob')
+    count = 0
+    # range（）方法，当i大于0小于字符串s的长度的时候
+    for i in range (0,len(str)):
+        # 如果从str中截取3位字母长度的子串，当等于‘bob’时，将计数加1
+        if str[i:i+3] == 'bob' :
+            count += 1
+    return count 
 # 测试
 str = 'azcbobobegghakl'
 print ("Number of times bob occurs is: " , num_bob(str))
@@ -37,6 +43,5 @@ print ("Longest substring in alphabetical order is: " , longest_substring(s))
 s = 'abcbcd'  
 print ("Longest substring in alphabetical order is: " , longest_substring(s))
    
-
 
 
