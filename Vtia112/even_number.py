7af10b215f136dc053f752063659d998c7be597a
Houjing.wei <weihoujing@163.com>
2018-05-08 19:45:36 +0800

    函数已经全部写完了，才想起来要分步commit。我下次注意，老斯。 %>_<%

---
+++ Vtia112/even_number.py
@@ -0,0 +1,13 @@
+#定义一个判断数字奇偶性的函数
+def even_odd(x):
+    if x % 2 == 0:
+        print('It\'s a even.')
+    else:
+        print('It\'s an odd number.')
+
+
+# 获取用户输入的数字
+n = input('请输入一个数字: ')
+
+#调用函数
+even_odd(n)
