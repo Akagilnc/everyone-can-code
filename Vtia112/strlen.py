751b082a23d07232c5894b91f59525d533ce4a9f
Houjing.wei <weihoujing@163.com>
2018-05-08 21:41:16 +0800

    第2次commit，实现了函数并通过测试。

--- Vtia112/strlen.py
+++ Vtia112/strlen.py
@@ -1,7 +1,14 @@
 #定义一个返回字符串长度的函数
+def strlen(s):
+
+    if isinstance(s, str) :
+        print('该字符串的长度为：%d' % len(s))
+    else:
+        print('Invalid string.')
 
 
 #获取用户输入
-
-
+#isstr= input('请输入一个字符串： ')
+var = 24324321
 #调用函数
+strlen(var)
