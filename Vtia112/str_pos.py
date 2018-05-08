8ecf9355e12ec0e7a081067b9340ac80726f1fcd
Houjing.wei <weihoujing@163.com>
2018-05-08 22:45:01 +0800

    第2次commit，实现了函数并完成测试。

--- Vtia112/str_pos.py
+++ Vtia112/str_pos.py
@@ -1,9 +1,22 @@
+
 #定义一个判断一个字符串是否包含子串的函数
+def find_str(s,t):
+    if t in s:
+    #try:
+        result = s.index(t)
+        print(result)
+    else:
+    #except:
+        return False
 
 
 #设置3个字符串，要求2个子串中，1个在母字符串中，1个不在。
-
+str1 = 'The best way of keeping a secret is to pretend there is not one.'
+str2 = 'keeping a secret'
+str3 = 'No one keeps no secrets.'
 
 #调用函数
+find_str(str1,str3)
+find_str(str1,str2)
 
 
