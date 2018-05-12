d42d08b24b6618e58d7a6e8c218c15c08f6e0c69
Houjing.Wei <weihoujing@163.com>
2018-05-12 14:38:08 +0800

    2nd commit，completed cube_root function and tested them. One of them is incorrect, please tell where the mistake is. Thank you.

---
+++ Vita112/homework_2/cube_root_2.py
@@ -0,0 +1,17 @@
+
+
+# 定义一个求任意数g的立方根x的函数
+def cube_root_2(g):
+    # 规定x的初始值，精度以及计算次数
+    x, s, n = g, 0.000000001, 1
+
+    # 当 x**3!= g 且 x**3-g 的结果大于要求精度时，求解过程
+    while abs(x**3-g) >= s:
+        x = 2*x/3 + g/(3*x*x)  # 老斯，牛顿迭代法的原理没有搞懂，求讲解。
+        print(x, n)
+        n += 1
+    return x
+
+# 调用函数
+answer = cube_root_2(-25)
+print(answer)
