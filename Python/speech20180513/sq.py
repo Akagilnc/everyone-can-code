117985b42245750aa6666ecf59b2466bcceca2a0
DeronLee <akagilnc@gmail.com>
2018-05-13 21:17:57 +0900

    add speech 2018 05 13

---
+++ Python/speech20180513/sq.py
@@ -0,0 +1,14 @@
+# 获取用户的输入g
+g = int(input('please input a number : '))
+# 设置初始猜测的平方根为x = 3
+x = 1
+n = 1
+# 当x的平方不等于g时，重复以下步骤
+while x**2 != g:
+    # 求平方根的公式，x = (g/x + x)/2
+    x = (g / x + x) / 2
+    print(x, n)
+    n += 1
+
+# 返回x
+print(x)
