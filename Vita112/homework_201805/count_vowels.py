3191b6b1c4bbda4181320fa74bb4b6a1c8e60f55
Houjing.Wei <weihoujing@163.com>
2018-05-19 15:20:25 +0800

    2nd commit，completed these 2 functions and tested them.

--- Vita112/homework_201805/count_vowels.py
+++ Vita112/homework_201805/count_vowels.py
@@ -2,7 +2,18 @@
 
 # 定义一个统计字符串str中的元音数量的函数count_vowels
 
+def count_vowels(str):
+    result = [ ]
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for letter in str:
+        if letter in vowels:
+            result.append(letter)
+
+    return len(result)
+
 # 获取输入的小写英文字母字符串
+temp_str = input('请输入一组小写英文字母： ')
 
 # 调用函数
+print(count_vowels(temp_str))
 
