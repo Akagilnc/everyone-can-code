b308a8cfcb534847c75b4b2a842d357a363d1e89
DeronLee <akagilnc@gmail.com>
2018-05-31 21:38:15 +0900

    lesson5

---
+++ Python/lesson_5_game/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, job, hp, weapon, mp):
+        self.name = name
+        self.job = job
+        self.hp = hp
+        self.weapon = weapon
+        self.mp = mp
---
+++ Python/lesson_5_game/weapon.py
@@ -0,0 +1,15 @@
+class Sword:
+    weapon_type = 'Sword'
+
+    def __init__(self, att, defence, name):
+        self.attack = att,
+        self.defence = defence
+        self.name = name
+
+
+class Hummer:
+    weapon_type = 'Hummer'
+
+    def __init__(self, att, defence):
+        self.attack = att
+        self.defence = defence
