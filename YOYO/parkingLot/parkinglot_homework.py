baae331afb8a0329cb5ef862ba97421fdff8f701
YOYO1HU9 <121189275@qq.com>
2018-09-06 12:28:35 +0800

    'correction'

--- YOYO/parkingLot/parkinglot_homework.py
+++ YOYO/parkingLot/parkinglot_homework.py
@@ -3,39 +3,51 @@ class ParkingLot:
     def __init__(self,capacity):
         self.capacity = capacity
         self.empty = capacity
-        self.park = []
+        self.seat = []
 
     def parking(self,car):
-        self.park.append(car)
-        self.empty -= 1
+        if car:
+            self.seat.append(car)
+            self.empty -= 1
 
     def check(self,car):
-        if car in self.park:
+        if car in self.seat:
             raise ValueError('This car has already in the park.')
 
     def take_out(self,car):
-        self.park.remove(car)
+        self.seat.remove(car)
         self.empty += 1
 
 class Car:
     pass
 
 class ParkingBoy:
-    def __init__(self,park):
-        self.park = park
+
+    def __init__(self,parkinglotlist):
+        self.parkinglotlist = parkinglotlist
+        if type(parkinglotlist) != list:
+            raise TypeError('This is not a list!')
 
     def parking(self, car):
-        self.park.append(car)
+        self.parkinglotlist.parking(car)
 
     def take_out(self, car):
-        self.park.remove(car)
+        self.parkinglotlist.take_out(car)
 
 
-SUV = Car()
+BMW = Car()
+Auto = Car()
+
 yoyo_park = ParkingLot(20)
-yoyo = ParkingBoy
+momo_park = ParkingLot(10)
+lolo_park = ParkingLot(50)
+yoyo = ParkingBoy([yoyo_park, momo_park, lolo_park])
+
+yoyo_park.parking(BMW)
+momo_park.parking(Auto)
 
 
+print(yoyo.parkinglotlist)
 
 
 
