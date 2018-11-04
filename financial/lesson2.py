fa697177f19a11d6c29477ba3e82c99ecdfc11ac
DeronLee <akagilnc@gmail.com>
2018-11-04 14:01:01 +0900

    fixed bugs

--- financial/lesson2.py
+++ financial/lesson2.py
@@ -1,15 +1,16 @@
-from lesson1 import even_or_odd
+from financial.lesson1 import even_or_odd
 import pandas as pd
 import numpy.random as random
 import attr
+
 
 @attr.s
 class Number:
     value = attr.ib()
     even_or_odd = attr.ib(default=None)
 
-def get_numbers():
 
+def get_numbers():
     number_list = []
     # 设置一组10个随机数字
     for i in random.randint(10000, size=10):
@@ -23,7 +24,9 @@ def get_numbers():
         number_list.append(number)
     print(number_list)
 
+
 get_numbers()
+
 
 class News:
     def __init__(self, news_date, news_content):
@@ -34,9 +37,10 @@ class News:
         return "{}(news_date = {}, news_content = {}) \n".format(
             self.__class__.__name__, self.news_date, self.news_content)
 
-# news_list = []
-# df = pd.read_csv('RedditNews.csv')
-# for news in df.get_values():
-#     news_list.append(News(news[0], news[1]))
 
-# print(news_list)
+news_list = []
+df = pd.read_csv('RedditNews.csv')
+for news in df.get_values():
+    news_list.append(News(news[0], news[1]))
+
+print(news_list)
